35e0efa20722f62e9dda83e0b8cc3398c50fcb79
yubin <yubin890816@163.com>
2022-02-02 21:16:09 +0800

    幂等性说明

--- src/main/java/com/yubin/analysis/idea_debug/MainTest.java
+++ src/main/java/com/yubin/analysis/idea_debug/MainTest.java
@@ -1,5 +1,11 @@
 package com.yubin.analysis.idea_debug;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MainTest {
 
     private static boolean ready;
@@ -13,10 +19,30 @@ public class MainTest {
             System.out.println("字段断点在被访问时触发了断点");
         }
         print(2);
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        List<Integer> collect = list.stream().skip(1).limit(8).filter(x -> x.intValue() > 3).collect(Collectors.toList());
+        System.out.println(collect);
     }
 
     private static void print(int a) {
         int b = a + 2;
         System.out.println(b);
+    }
+
+    @Test
+    public void testExceptionBreakpoint() {
+        try {
+            Integer a = null;
+            if (true) {
+                throw new NullPointerException();
+            }
+            System.out.println(a.intValue());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
